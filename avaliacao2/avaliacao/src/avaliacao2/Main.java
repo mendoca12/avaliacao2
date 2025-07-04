e72e7a6f15963383ea79bfcebc555068acf3fff4
mendoca <mendoca2308@gmail.com>
2025-07-04 20:34:17 -0300

    Atualizacao do codigo

---
+++ avaliacao2/avaliacao/src/avaliacao2/Main.java
@@ -0,0 +1,36 @@
+package avaliacao2;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		List<Livro> livros = new ArrayList<>()
+				Scanner scanner = new Scanner(System.in)
+				int opcao;
+		
+		do {
+			System out.println("======================MENU============");
+			System.out.println("1.Cadastrar novo Livro");
+			System.out.println("2.Cadastrar novo livro");
+			System.out.println("3. Cadastrar novo livro");
+			System.out.println("4. Cadastrar novo livro");
+			System.out.println("5.Cadastrar novo livro");
+			
+			System.out.println("0.Sair");
+			System.out.print("Escolha uma opção:");
+			opcao = scanner.nextlnt();
+			System.out.println();
+			  
+			switch (opcao) {
+			case 1:
+				System.out.print("");
+		 
+		}
+			
+			
+			
+		}
+          
+	}
+
+}
